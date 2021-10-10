d9111668769c39a538a32fe237dcb9f9f219d563
HarshKumar04 <kumarharsh1367@gmail.com>
2021-10-11 02:11:25 +0530

    add:  more functionallity

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const btnTranslate = document.querySelector("#btn-translate");
 const inputBox = document.querySelector("#input-box");
-btnTranslate.addEventListener("click",function clickHandler(){
+const outputBox = document.querySelector("#output-box");
+
+const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function urlMaker(text){
+    return url + "?" +"text=" + text;        
+}
+function clickHandler(){
     var userInput = inputBox.value;
-    console.log(userInput);
-})
+    // outputBox.innerText = userInput+ ":thhhis is the yourre transslation";
+    fetch(urlMaker(userInput))
+    
+     
+}
+btnTranslate.addEventListener("click",clickHandler);
