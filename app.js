d3e7bf55f2ce8bc375ea5b40ff926554dc7264d6
HarshKumar04 <kumarharsh1367@gmail.com>
2021-10-11 01:04:48 +0530

    update both app,js and index.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const btnTranslate = document.querySelector("#btn-translate");
-
+const inputBox = document.querySelector("#input-box");
 btnTranslate.addEventListener("click",function clickHandler(){
-    console.log("clicked!");
+    var userInput = inputBox.value;
+    console.log(userInput);
 })
