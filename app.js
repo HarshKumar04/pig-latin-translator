749a7af870fd2cc26ca12af12478c1852a90a3ae
HarshKumar04 <kumarharsh1367@gmail.com>
2021-10-11 02:44:20 +0530

    app done

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ const btnTranslate = document.querySelector("#btn-translate");
 const inputBox = document.querySelector("#input-box");
 const outputBox = document.querySelector("#output-box");
 
-const url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+const url = "https://api.funtranslations.com/translate/pig-latin.json";
 
 function urlMaker(text){
     return url + "?" +"text=" + text;        
 }
+
+function errorHandler(error){
+    alert("Something wrong with server please try agian after sometime!!");
+    console.log(error);
+}
+
 function clickHandler(){
     var userInput = inputBox.value;
-    // outputBox.innerText = userInput+ ":thhhis is the yourre transslation";
     fetch(urlMaker(userInput))
-    
-     
+    .then(response => response.json())
+    .then(json => {
+        var translatedTextValue = json.contents.translated;
+        outputBox.innerText = translatedTextValue;
+
+    })
+    .catch(errorHandler)
 }
 btnTranslate.addEventListener("click",clickHandler);
